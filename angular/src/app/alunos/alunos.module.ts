ed2f85eba475e91d26a7395751d545370472a700
isaac <izakew19@gmail.com>
2023-11-21 08:13:52 -0300

    Primeiro commit

---
+++ angular/src/app/alunos/alunos.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {AlunosComponent} from "./alunos.component";
+import { AlunoFormComponent } from './aluno-form/aluno-form.component';
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
+import {AlunosRountingModule} from "./alunos.rounting.module";
+import {AlunosService} from "./alunos.service";
+import {SharedModule} from "../shared/shared.module";
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  imports: [CommonModule, AlunosRountingModule, SharedModule, FormsModule],
+  declarations: [AlunosComponent, AlunoFormComponent, AlunoDetalheComponent],
+  exports: [],
+  providers: [AlunosService]
+})
+
+export class AlunosModule {}
