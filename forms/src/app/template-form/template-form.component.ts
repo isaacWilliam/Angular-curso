41a25e5d460fde853a0c19b62d94846bca3aa6ea
isaac <izakew19@gmail.com>
2023-11-24 17:55:24 -0300

    Configurando as telas.

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-template-form',
@@ -8,8 +7,22 @@ import {FormControl, FormGroup} from "@angular/forms";
 })
 export class TemplateFormComponent {
 
+  // usuario = {
+  //   nome: 'Estevão',
+  //   email: 'estev@gmail.com'
+  // }
+  usuario = {
+    nome: '',
+    email: '',
+    cep: '',
+    rua: '',
+    numero: null,
+    bairro: '',
+    uf: '',
+    cidade: '',
+    complemento: ''
+  }
   onSubmit(form?: any){
     console.log(form)
   }
-
 }
